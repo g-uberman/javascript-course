eba51b41caf4f09a5bbb333ab0544c11fa6d386d
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-25 18:18:23 +0200

    creating new objects

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
-var myString = new String();         // create new empty string
-myString = "hello";
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-console.log(myString.length);
-console.log(myString.toUpperCase());
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+
+console.log(myCar.driver);
+
+myCar.drive = function(){
+    console.log("now driving");
+};
+
+myCar.drive();
+
+var myCar2 = {
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+        console.log(speed * time);
+    }
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50,3);         // 150 travelled
