5969a5a2d25241a028165b5cbca65419fde21b44
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-25 17:29:56 +0200

    objects

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-var myArray = [];       // created empty array
+var myString = new String();         // create new empty string
+myString = "hello";
 
-myArray[0] = 25;        // assigned number at 1st position
-myArray[1] = 35;
-myArray[2] = true;
-myArray[3] = "hello";
-
-console.log(myArray);
-
-var myOtherArray = [10,20,"hi",false];
-console.log(myOtherArray);
-
-var myThirdArray = new Array(5)     // created array with 5 empty slots
-
-console.log(myArray.length);        // number of elements
-myArray.sort                        // sort lowest to highest or alphabetically
+console.log(myString.length);
+console.log(myString.toUpperCase());
