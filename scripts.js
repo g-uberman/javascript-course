e86afa62c16108fd11cd2715094573a1870017ee
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-25 15:57:53 +0200

    functions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-var links = document.getElementsByTagName("a");
+function getAverage(a,b,c,d,e,f){
 
-for(i = 0; i < links.length; i++){
-    links[i].className = "link-" + i;
+    var average = (a + b + c + d + e + f) / 6;
+    console.log(average)
+    return average;         //save var average outside function
+
 }
+
+var myResult = getAverage(7,12,100,77,23,60);   //third value ignored
+console.log("the average is " + myResult);
