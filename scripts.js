56b60a003e48f3bd7be278545fef23b9e9d07452
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-25 14:34:49 +0200

    else if statements

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
-var youLikeMeat = false;
-var myNum = 8;
+var myAge = 35;
 
-if (myNum == 10){
-
-    document.write("myNum is equal to 10");
-
-} else{
-
-    document.write("myNum is not equal to 10");
-
+if (myAge > 40){
+    document.write("You are over 40!");
+} else if (myAge > 30){
+    document.write("You are over 30!");
+} else if (myAge > 20){
+    document.write("You are over 20!");
+} else if (myAge > 10){
+    document.write("You are over 10!");
+} else {
+    document.write("You not over 10!");
 }
