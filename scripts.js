f97a28aae355db13094c1dbfaabd90e15d905e36
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-26 18:39:56 +0200

    onclick show more button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-document.getElementsByClassName("content");
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+// SHOW MORE BUTTON
+button.onclick = function () {
+    if (content.className == "open") {
+        //shrink box
+        content.className = "";
+        button.innerHTML = "Show More";
+    } else {
+        //expand box
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+};
