a63a7842c4eb3d158bc4553ece6d946dba86c105
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-26 18:12:26 +0200

    added style.css

--- scripts.js
+++ scripts.js
@@ -1,17 +1,1 @@
-var birthday = new Date(1987, 4, 16, 3, 29, 15);
-var birthday1 = new Date(1987, 4, 16, 3, 29, 15);
-console.log(birthday);
-
-console.log(birthday.getMonth());
-console.log(birthday.getFullYear());
-console.log(birthday.getDate());
-console.log(birthday.getDay());     // day of the week (0-6)
-console.log(birthday.getTime());    // number of milliseconds since Jan 1st 1970
-console.log(birthday.getHours());
-
-// compare dates by getTime:
-if(birthday.getTime() == birthday1.getTime()){
-    console.log("birthdays are equal");
-} else{
-    console.log("birthdays are not equal");
-}
+document.getElementsByClassName("content");
