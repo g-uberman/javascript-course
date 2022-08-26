7fc976681caeedd821e63032351e2e499160b578
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-26 13:41:00 +0200

    constructor functions

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
-var myCar = {
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){
+// constructor function for making new objects:
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
         console.log(speed * time);
     },
-    logDriver: function(){
+    this.logDriver = function(){
         console.log("Driver name is " + this.driver);
-    },
-    test: function(){
+    }
+    this.test =  function(){
         console.log(this);
     }
-};
+}
 
-myCar.logDriver();
-console.log(myCar.maxSpeed);
-myCar.drive(50,3);         // 150 travelled
-myCar.test();
+// create new objects using Car function:
+var myCar0 = new Car (100, "Shaun");
+var myCar1 = new Car (120, "Mary");
+var myCar2 = new Car (160, "Willy");
+var myCar3 = new Car (90, "Bob");
+
+// print properties for testing:
+myCar0.test();
+myCar1.test();
+myCar2.test();
+myCar3.test();
