e6782beeb6e166902e6c96c78f159d2ae0570922
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-26 13:49:39 +0200

    date object

--- scripts.js
+++ scripts.js
@@ -1,26 +1,10 @@
-// constructor function for making new objects:
-var Car = function(maxSpeed, driver){
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time);
-    },
-    this.logDriver = function(){
-        console.log("Driver name is " + this.driver);
-    }
-    this.test =  function(){
-        console.log(this);
-    }
-}
+// log current date & time:
+var myDate = new Date();
+console.log(myDate);
 
-// create new objects using Car function:
-var myCar0 = new Car (100, "Shaun");
-var myCar1 = new Car (120, "Mary");
-var myCar2 = new Car (160, "Willy");
-var myCar3 = new Car (90, "Bob");
+// create date object (year, month, day, hours, minutes, seconds):
+var myPastDate = new Date(1545, 11, 2, 10, 30, 15);
+var myFutureDate = new Date(40000, 0, 28);
 
-// print properties for testing:
-myCar0.test();
-myCar1.test();
-myCar2.test();
-myCar3.test();
+console.log(myPastDate);
+console.log(myFutureDate);
