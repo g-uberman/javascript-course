88ad3b60dd36e60813381e295a1609be786ed3a9
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-28 12:47:13 +0200

    window onload event

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+//ONCLICK EVENT ONLY SETUP BY A FUNCTION, NOT BY DEFAULT
+function setUpEvents() {
 
-// SHOW MORE BUTTON
-button.onclick = function () {
-    if (content.className == "open") {
-        //shrink box
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else {
-        //expand box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-    }
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+
+    // SHOW MORE BUTTON
+    button.onclick = function () {
+        if (content.className == "open") {
+            //shrink box
+            content.className = "";
+            button.innerHTML = "Show More";
+        } else {
+            //expand box
+            content.className = "open";
+            button.innerHTML = "Show Less";
+        }
+    };
+
+};
+
+//AUTORUN SETUP FUNCTION AFTER PAGE IS FULLY LOADED
+//makes sure that the needed elements are already in place
+window.onload = function () {
+    setUpEvents();
 };
